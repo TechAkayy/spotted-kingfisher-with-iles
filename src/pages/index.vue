cbb4d76993a6149003218f73d3593e5d88b556c2
Akayy <techakayy@gmail.com>
2023-03-25 01:33:29 +1100

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<page>
+  title: Iles App
+</page>
+
+<template>
+  <div>
+    <HeroSection client:load /><TheVolunteering client:visible /><GetInvolved
+      client:visible
+    /><DiscoverProjects client:visible />
+  </div>
+</template>
